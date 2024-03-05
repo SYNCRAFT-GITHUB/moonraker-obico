c05859097a17c8b7905dabde13e2385555f936e4
RafaelSwi <rafanswi@protonmail.com>
2024-03-05 11:08:36 -0300

    update remove eof in install.sh

--- install.sh
+++ install.sh
@@ -68,22 +68,6 @@ prompt_for_settings() {
 
 cat <<EOF
 
-We need info about your Moonraker. If you are not sure, just leave them as defaults.
-
-EOF
-
-  read -p "Moonraker host: " -e -i "${MOONRAKER_HOST}" user_input
-  eval MOONRAKER_HOST="${user_input}"
-  read -p "Moonraker port: " -e -i "${MOONRAKER_PORT}" user_input
-  eval MOONRAKER_PORT="${user_input}"
-  read -p "Moonraker config file: " -e -i "${MOONRAKER_CONFIG_FILE}" user_input
-  eval MOONRAKER_CONFIG_FILE="${user_input}"
-  MOONRAKER_CONF_DIR=$(dirname "${MOONRAKER_CONFIG_FILE}")
-  read -p "Klipper log directory: " -e -i "${MOONRAKER_LOG_DIR}" user_input
-  eval MOONRAKER_LOG_DIR="${user_input}"
-  echo ""
-}
-
 ensure_deps() {
   report_status "Installing required system packages... You may be prompted to enter password."
 
